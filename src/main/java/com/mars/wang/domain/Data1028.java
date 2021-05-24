ff1396d7f9f75b4419632d2ca160ecfc73d18e21
Administrator <2722918483@qq.com>
2021-05-24 10:12:26 +0800

    update setShipTo

--- src/main/java/com/mars/wang/domain/Data1028.java
+++ src/main/java/com/mars/wang/domain/Data1028.java
@@ -112,11 +112,13 @@ public class Data1028 extends  ParentData implements Serializable {
     @Override
     public void setShipToP() {
         String code=this.consigneekey;
+        System.out.println("000"+this.consigneekey.indexOf("000"));
 
         if (this.consigneekey.indexOf("000")==0){
             code = consigneekey.replace("000","");
 
         }
+
         super.dataPrediction.setShipToP(code);
     }
     //收货地址
